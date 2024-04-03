ce67ce91cc0cddd71328107383402604f279bf2d
Giselle <80613431+KYUKAO@users.noreply.github.com>
2024-04-03 20:21:30 +0800

    初步完成雾气，闪电还未完全跟随

---
+++ Assets/ParticleEffectController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ParticleEffectController : MonoBehaviour
+{
+    // 暴露的粒子系统参数，允许你在Unity编辑器中指定哪个粒子效果将被控制
+    public ParticleSystem particleEffect;
+
+    void OnEnable()
+    {
+        // 当游戏对象被激活时，播放粒子效果
+        if (particleEffect != null)
+        {
+            particleEffect.Play();
+        }
+    }
+
+    void OnDisable()
+    {
+        // 当游戏对象被禁用时，停止粒子效果
+        if (particleEffect != null)
+        {
+            particleEffect.Stop();
+        }
+    }
+}
+
